bde55e133af44bba188cf96677f078ff4994077d
Megan Adams <meganoliviawendyadams@gmail.com>
2019-08-06 14:59:08 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -3,6 +3,15 @@ class Dog
   
   attr_accessor :name, :breed, :age 
   
+  
+  def initialize(name, breed, age)
+    
+    @name = name 
+    @breed = breed 
+    @age = age 
+    
+  end
+  
 #should hav name, breed and age attributes
 
 #keep track of each instance of dog created
