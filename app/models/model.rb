0e4f418b002f29853fb31aa5a6138f440473a534
Megan Adams <meganoliviawendyadams@gmail.com>
2019-08-06 14:06:06 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,9 @@
-class Model
+class Model #put dog here
+
+#should hav name, breed and age attributes
+
+#keep track of each instance of dog created
+
+#and class method 'all' to return those  instances
 
 end
